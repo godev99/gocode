81fbdaccb12dc5d4db4d3ce384c924c31700e154
senavi <gab@fastmail.net>
2017-01-20 00:49:19 +0100

    chp1/ex3
    Signed-off-by: Gabriel <gab@fastmail.net>

--- chp1/ex3.go
+++ chp1/ex3.go
@@ -3,13 +3,55 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
+	start := time.Now()
+	ch := make(chan string)
+	go echo1(ch)
+	go echo2(ch)
+	go echo3(ch)
 
-	for i := 1; i < len(os.Args); i++ {
-		fmt.Printf("index : %d	", i)
-		fmt.Printf("value : %s\n", os.Args[i])
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+
+	fmt.Printf("%.15fs elapsed\n", time.Since(start).Seconds())
+}
+
+func echo1(ch chan<- string) {
+
+	start := time.Now()
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
 	}
+	secs := time.Since(start).Seconds()
+	fmt.Println(s)
+	ch <- fmt.Sprintf("echo1 -> %.30fs", secs)
 
+}
+
+func echo2(ch chan<- string) {
+
+	start := time.Now()
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("echo2 -> %.30fs", secs)
+
+}
+
+func echo3(ch chan<- string) {
+	start := time.Now()
+	fmt.Println(strings.Join(os.Args[1:], " "))
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("echo3 -> %.30fs", secs)
 }
